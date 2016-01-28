49375fd5431a303e1752a6319610092a508cf3a7
John Cairns <jcairns@conversantmedia.com>
2016-01-28 14:45:59 -0600

    Fixing whitespace

--- src/main/java/com/conversantmedia/util/collection/Stack.java
+++ src/main/java/com/conversantmedia/util/collection/Stack.java
@@ -26,58 +26,58 @@ package com.conversantmedia.util.collection;
 public interface Stack<N> {
 
 
-	boolean contains(N n);
+    boolean contains(N n);
 
-	/**
-	 * Add the element to the stack top, optionally failing if there is
-	 * no capacity
-	 *
-	 * @param n
-	 * @return
-	 */
-	boolean add(N n);
-
-	/**
-	 * push the next element on the stack top - waiting if needed for space to become available
-	 *
-	 * @param n
-	 */
-	void push(N n);
-
-	/**
-	 * show the current stack top
-	 * @return
-	 */
-	N peek();
-
-	/**
-	 * pop and return the element from the top of the stack
+    /**
+     * Add the element to the stack top, optionally failing if there is
+     * no capacity
      *
-	 * @return N - the element, or null if the stack is empty
-	 */
-	N pop();
+     * @param n
+     * @return
+     */
+    boolean add(N n);
 
-	/**
-	 * return the size of the stack in number of elements
-	 *
-	 * @return
-	 */
-	int size();
+    /**
+     * push the next element on the stack top - waiting if needed for space to become available
+     *
+     * @param n
+     */
+    void push(N n);
 
-	/**
-	 * return the number of empty slots available in the stack
-	 *
-	 * @return
-	 */
-	int remainingCapacity();
+    /**
+     * show the current stack top
+     * @return
+     */
+    N peek();
 
-	/**
-	 * @return boolean - true if stack is empty
-	 */
-	boolean isEmpty();
+    /**
+     * pop and return the element from the top of the stack
+     *
+     * @return N - the element, or null if the stack is empty
+     */
+    N pop();
 
-	/**
-	 * clear the stack
-	 */
-	void clear();
+    /**
+     * return the size of the stack in number of elements
+     *
+     * @return
+     */
+    int size();
+
+    /**
+     * return the number of empty slots available in the stack
+     *
+     * @return
+     */
+    int remainingCapacity();
+
+    /**
+     * @return boolean - true if stack is empty
+     */
+    boolean isEmpty();
+
+    /**
+     * clear the stack
+     */
+    void clear();
 }
