313c64450bcf42f7d361e3c01bd1573517e6ab36
John Cairns <jcairns@conversantmedia.com>
2016-04-20 08:44:19 -0500

    Adding MPMC bounded queues based on Dmitry Vyukov http://www.1024cores.net/home/lock-free-algorithms/queues/bounded-mpmc-queue

---
+++ src/test/java/com/conversantmedia/util/concurrent/MPMCConcurrentQueuePerfTest.java
@@ -0,0 +1,57 @@
+package com.conversantmedia.util.concurrent;
+
+/*
+ * #%L
+ * Conversant Disruptor
+ * ~~
+ * Conversantmedia.com © 2016, Conversant, Inc. Conversant® is a trademark of Conversant, Inc.
+ * ~~
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.conversantmedia.util.estimation.Percentile;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by jcairns on 5/28/14.
+ */
+@Ignore
+public class MPMCConcurrentQueuePerfTest {
+
+    @Test
+    public void testPerformance() throws InterruptedException, Percentile.InsufficientSamplesException {
+        System.out.println("1x1 using MultiThread");
+        ConcurrentQueuePerformanceTest.testPerformance(new MPMCConcurrentQueue<Integer>(1024));
+    }
+
+
+    @Test
+    public void testRate() throws InterruptedException, Percentile.InsufficientSamplesException {
+        ConcurrentQueuePerformanceTest.testRate(new MPMCConcurrentQueue<Integer>(1024));
+    }
+
+    @Ignore
+    public void testNThreadPerformance() throws Percentile.InsufficientSamplesException, InterruptedException {
+        System.out.println("NThread in MultiThread");
+        ConcurrentQueuePerformanceTest.testNThreadPerformance(new MPMCConcurrentQueue<Integer>(1024));
+    }
+
+    @Test
+    public void testNxNThreadPerformance() throws Percentile.InsufficientSamplesException, InterruptedException {
+        System.out.println("NThread in MultiThread");
+        ConcurrentQueuePerformanceTest.testNxNThreadPerformance(new MPMCConcurrentQueue<Integer>(1024));
+    }
+
+}
