06415b28b1d817cb0894cc5b32c123ca2351390a
John Cairns <john@2ad.com>
2020-03-19 15:45:06 -0500

    15: support java 11, add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.conversantmedia.disruptor {
+    opens com.conversantmedia.util.collection;
+    opens com.conversantmedia.util.concurrent;
+    opens com.conversantmedia.util.estimation;
+}
