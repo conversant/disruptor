6b6709e7239ee05ccdf0cf94b068491f564b26bd
John Cairns <john@2ad.com>
2021-01-06 14:00:56 -0600

    fix typo

--- src/main/java/com/conversantmedia/util/concurrent/SpinPolicy.java
+++ src/main/java/com/conversantmedia/util/concurrent/SpinPolicy.java
@@ -7,7 +7,7 @@ package com.conversantmedia.util.concurrent;
  *           In general, "WAITING" performs better than BLOCKING
  * BLOCKING - Java style synchronization, fair and well behaved on the processor.   This is very slow and
  *            undercuts some of the advantage of using the Disruptor in the first place.
- * SPINNING - For extreme performance senarios where processor utilization is not a concern, this will slightly
+ * SPINNING - For extreme performance scenarios where processor utilization is not a concern, this will slightly
  *            lower latency over WAITING but at a cost of spin locking the CPU.
  *
  * Created by jcairns on 2/18/16.
