0803a76016d3255ef503100ed1d3bc3a17183331
John Cairns <john@2ad.com>
2022-10-18 10:46:51 -0500

    1.2.21: dependabot changes

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 VERSION=$(date +%s)
 PROJECT=jac18281828/$(basename ${PWD})
 
-docker build . -t ${PROJECT}:${VERSION} && \
+docker build --no-cache=true . -t ${PROJECT}:${VERSION} && \
     docker run -v ${PWD}:/build --rm -i -t ${PROJECT}:${VERSION}
